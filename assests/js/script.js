7ee124d22254d0b38b54c98d8b86d7731165360a
Ömer Faruk Eren <faruk008887@gmail.com>
2023-10-13 17:33:28 +0300

    Update script.js

--- assests/js/script.js
+++ assests/js/script.js
@@ -53,7 +53,7 @@ function(){
 
 // <!-- typed js effect starts -->
     var typed = new Typed(".typing-text", {
-        strings: ["Cyber Security Specialist", "Pentester", "Fal Uzmanı", "Aşko"],
+        strings: ["Cyber Security Specialist", "Pentester", "Fal Uzmanı", "Aşko Kuşko"],
         loop: true,
         typeSpeed: 50,
 		backSpeed: 25,
